3f4312e25ddc0fab4b94c347e742fe05534e66da
Marcus Noble <github@marcusnoble.co.uk>
2023-03-31 08:45:48 +0100

    Change version override to use a singlle env var
    Signed-off-by: Marcus Noble <github@marcusnoble.co.uk>

--- pkg/application/doc.go
+++ pkg/application/doc.go
@@ -26,18 +26,16 @@
 // When specifing the App version there are a couple special cases that you can take advantage of:
 //  1. Using the value `latest` as the App version will cause the latest released version found on GitHub to be used.
 //  2. Setting the version to an empty string will allow for overriding the version from an environment variable.
-//     If an environment variable is found with the prefix `E2E_OVERRIDE_` followed by an uppercase version of the app
-//     name (with dashes replaced with underscored) then that value will be used. If no such environemnt variable is
-//     found then it will fallback to the same logic as `latest` above.
-//
-// E.g. To override the `cluster-aws` app version the environment variable `E2E_OVERRIDE_CLUSTER_AWS` should be used.
+//     The environment variable `E2E_OVERRIDE_VERSIONS` can be used to provide a comma seperated list of app version
+//     overrides in the format `app-name=version` (e.g. `cluster-aws=v1.2.3,cluster-gcp=v1.2.3-2hehdu`). If no such
+//     environemnt variable is found then it will fallback to the same logic as `latest` above.
 //
 // Combining these two features together allows for creating scenarios that test upgrading an App from the current latest
 // to the version being worked on in a PR.
 //
 // Example:
 //
-// Assuming the `E2E_OVERRIDE_CLUSTER_AWS` env var is set to a valid version then the following will install cluster-aws
+// Assuming the `E2E_OVERRIDE_VERSIONS` env var is set to override cluster-aws with a valid version then the following will install cluster-aws
 // as the lastest released version then later install (upgrade) again with the version overridden from the environment variable.
 //
 //	appCR, configMap, err := application.New("upgrade-test", "cluster-aws").WithVersion("latest").Build()

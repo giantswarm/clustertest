abf0efa6bffb1aa4abe0f71651745d4cd3111099
Mario Nitchev <marionitchev@gmail.com>
2023-06-01 10:20:42 +0300

    Add doc comments to DoesNotHaveLabels

--- pkg/client/options.go
+++ pkg/client/options.go
@@ -6,8 +6,11 @@ import (
 	cr "sigs.k8s.io/controller-runtime/pkg/client"
 )
 
+// DoesNotHaveLabels filters the list/delete operation for objects that don't
+// have the given set of labels.
 type DoesNotHaveLabels []string
 
+// ApplyToList applies this configuration to the given list options.
 func (m DoesNotHaveLabels) ApplyToList(opts *cr.ListOptions) {
 	selector := labels.NewSelector()
 	for _, label := range m {
@@ -19,6 +22,7 @@ func (m DoesNotHaveLabels) ApplyToList(opts *cr.ListOptions) {
 	opts.LabelSelector = selector
 }
 
+// ApplyToDeleteAllOf applies this configuration to the given an List options.
 func (m DoesNotHaveLabels) ApplyToDeleteAllOf(opts *cr.DeleteAllOfOptions) {
 	m.ApplyToList(&opts.ListOptions)
 }

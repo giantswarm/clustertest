cbe2175b778c42a09c7536165dd7c7242c67761b
Jose Armesto <github@armesto.net>
2023-08-23 11:31:38 +0200

    Updated examples in docs

--- pkg/application/doc.go
+++ pkg/application/doc.go
@@ -1,4 +1,4 @@
-// package application provides wrapper types around the concept of an App and its associated values ConfigMap.
+// Package application provides wrapper types around the concept of an App and its associated values ConfigMap.
 //
 // A standard [Application] type is available as well as a [Cluster] helper type that encapsulates both the
 // cluster app and the associated default-apps and their values ConfigMaps. The [Cluster] type also handles
@@ -7,7 +7,7 @@
 // # Creating an App
 //
 //	app := application.New("test-installation", "external-dns").
-//		WithNamespace("default").
+//		WithOrganization("giantswarm").
 //		WithVersion("").
 //		MustWithValuesFile("./test_data/externaldns_values.yaml", &application.ValuesTemplateVars{})
 //

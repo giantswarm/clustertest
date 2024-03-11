e86b9d8ef1d87326a2849d4c2e5675552d557be9
Christian Bianchi <whites11@gmail.com>
2024-03-11 13:25:06 +0100

    Add possibility to exec a command in a pod.

---
+++ pkg/client/exec.go
@@ -0,0 +1,59 @@
+package client
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+
+	"github.com/giantswarm/microerror"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/remotecommand"
+	"k8s.io/kubectl/pkg/scheme"
+
+	"github.com/giantswarm/clustertest/pkg/logger"
+)
+
+func (c *Client) ExecInPod(ctx context.Context, podName, namespace, containerName string, command []string) (string, string, error) {
+	logger.Log("Running %v in container %q in pod %q", command, containerName, podName)
+
+	tty := true
+
+	coreClient, err := kubernetes.NewForConfig(c.config)
+	if err != nil {
+		return "", "", microerror.Mask(err)
+	}
+
+	req := coreClient.CoreV1().RESTClient().Post().
+		Resource("pods").
+		Name(podName).
+		Namespace(namespace).
+		SubResource("exec").
+		Param("container", containerName)
+	req.VersionedParams(&corev1.PodExecOptions{
+		Container: containerName,
+		Command:   command,
+		Stdin:     false,
+		Stdout:    true,
+		Stderr:    true,
+		TTY:       tty,
+	}, scheme.ParameterCodec)
+
+	var stdout, stderr bytes.Buffer
+	exec, err := remotecommand.NewSPDYExecutor(c.config, "POST", req.URL())
+	if err != nil {
+		return "", "", fmt.Errorf("failed to exec command in pod - %v", err)
+	}
+
+	err = exec.StreamWithContext(ctx, remotecommand.StreamOptions{
+		Stdin:  nil,
+		Stdout: &stdout,
+		Stderr: &stderr,
+		Tty:    tty,
+	})
+	if err != nil {
+		return "", "", fmt.Errorf("failed to exec command in pod - %v", err)
+	}
+
+	return stdout.String(), stderr.String(), err
+}

4a44019cdb88c8068758d7492a53813d55c5e8b0
Marcus Noble <github@marcusnoble.co.uk>
2024-05-20 10:16:51 +0100

    Include PipelineRun and Taskrun name in App labels
    Signed-off-by: Marcus Noble <github@marcusnoble.co.uk>

--- pkg/application/utils.go
+++ pkg/application/utils.go
@@ -77,3 +77,14 @@ func getOverrideVersion(app string) (string, bool) {
 	ver := getOverrideVersions()[strings.ToLower(app)]
 	return ver, ver != ""
 }
+
+func mergeMaps(m1 map[string]string, m2 map[string]string) map[string]string {
+	merged := make(map[string]string)
+	for k, v := range m1 {
+		merged[k] = v
+	}
+	for key, value := range m2 {
+		merged[key] = value
+	}
+	return merged
+}
